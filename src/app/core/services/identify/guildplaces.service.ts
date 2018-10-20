0e799af513098e3718a68fb6c4e54ddf71b7fea7
Geoffrey Plateaux <gp@qeos.be>
2018-10-20 21:22:51 +0200

    Identifier

--- src/app/core/services/identify/guildplaces.service.ts
+++ src/app/core/services/identify/guildplaces.service.ts
@@ -1,4 +1,4 @@
-import {Injector, Injectable} from "@angular/core";
+import {Injectable, Injector} from "@angular/core";
 import {Observable} from 'rxjs/Rx';
 
 import {Service} from "../service";
@@ -9,169 +9,385 @@ import {LoginService} from "../login.service";
 @Injectable()
 export class GuildplacesService extends Service {
 
-  numerics = ["Id", "Poids", "Prix", "Qualite", "Taille"];
-  floats = [];
-  dates = [];
-  booleans = ["Identifie"];
-  carats = new Map([
-    [1, 2],
-    [2, 2.75],
-    [3, 3.5],
-    [4, 4.25],
-    [5, 5],
-    [-1, 1]
-  ]);
-  WeightToExpected = new Map([
-    ["Outil", new Map([
-      [1, {name: "Aiguille", stars: 0}],
-      [2, {name: "Poinçon", stars: 0}],
-      [3, {name: "Marteau de joaillier", stars: 0}],
-      [5, {name: "Ciseau à bois", stars: 0}],
-      [20, {name: "Marteau de forgeron / de tailleur", stars: 0}],
-      [40, {name: "Hache de bûcheron", stars: 3}],
-      [60, {name: "Creuset", stars: 0}]
-    ])],
-    ["Anneau", new Map([
-      [2, {name: "Anneau standard", stars: 3}]
-    ])],
-    ["Arme 1 Main", new Map([
-      [5, {name: "Coutelas (os, pierre) / Corchet / Dague / Griffes / Racine / Torche", stars: 1}],
-      [7.5, {name: "Epée de bois / Fouet / Gantelet", stars: 4}],
-      [10, {name: "Epée courte / Hachette / Gourdin", stars: 0}],
-      [15, {name: "Cimeterre en pierre / Épieu / Gourdin clouté / Hache de lancer / Kama / Lame / Lame d'os / Machette / Pic", stars: 2}],
-      [20, {name: "Épée longue / Hache d'armes / Masse d'armes", stars: 0}],
-      [25, {name: "Fléau d'armes", stars: 0}],
-      [30, {name: "Épée bâtarde / Hache de guerre naine / Marteau de guerre / Morgenstern", stars: 1}]
-    ])],
-    ["Arme 2 mains", new Map([
-      [8, {name: "Bâton lesté / Bâtons de parade", stars: 0}],
-      [10, {name: "Arc court", stars: 3}],
-      [15, {name: "Arc composite / Arc long / Bâton de combat / Hachoir en bois", stars: 4}],
-      [20, {name: "Bâton de feu", stars: 5}],
-      [30, {name: "Arbalète légère / Épée à deux mains / Grande hache / Hache de bataille / de guerre / en os", stars: 3}],
-      [35, {name: "Arbalète composite / Chaîne cloutée", stars: 4}],
-      [40, {name: "Massue trolle, Pioche, Vouge", stars: 2}],
-      [45, {name: "Arbalète lourde, Faux", stars: 4}],
-      [50, {name: "Vorpale", stars: 5}],
-      [60, {name: "Lance-feu", stars: 5}]
-    ])],
-    ["Armure", new Map([
-      [3, {name: "Vêtement léger", stars: 0}],
-      [5, {name: "Pagne en cuir", stars: 0}],
-      [7, {name: "Vêtement lourd", stars: 0}],
-      [12, {name: "Armure légère", stars: 1}],
-      [20, {name: "Armure intermédiaire", stars: 1}],
-      [40, {name: "Armure lourde", stars: 3}],
-      [60, {name: "Armure très lourde", stars: 2}]
-    ])],
-    ["Baguette", new Map([
-      [3, {name: "Baguette standard", stars: 1}]
-    ])],
-    ["Bijou", new Map([
-      [5, {name: "Bijou standard", stars: 3}]
-    ])],
-    ["Bottes", new Map([
-      [3, {name: "Jambières en fourrure / Sandales", stars: 0}],
-      [5, {name: "Bottes", stars: 0}],
-      [15, {name: "Jambières lourdes", stars: 3}]
-    ])],
-    ["Bouclier", new Map([
-      [5, {name: "Targe", stars: 0}],
-      [10, {name: "Bouclier léger", stars: 3}],
-      [20, {name: "Bouclier lourd", stars: 3}],
-      [35, {name: "Bouclier à pointes", stars: 5}]
-    ])],
-    ["Casque", new Map([
-      [3, {name: "Casque léger", stars: 0}],
-      [7, {name: "Casque à cornes", stars: 0}],
-      [10, {name: "Casque à pointes", stars: 3}],
-      [15, {name: "Heaume", stars: 3}]
-    ])],
-    ["Talisman", new Map([
-      [3, {name: "Talisman standard", stars: 2}],
-      [5, {name: "Gorgeron en métal", stars: 1}],
-      [6, {name: "Collier de dents", stars: 0}]
-    ])]
-  ])
-  NameToExpectedMaterial = new Map([
-    ["", ""]
-  ])
+	numerics = ["Id", "Poids", "Prix", "Qualite", "Taille"];
+	floats = [];
+	dates = [];
+	booleans = ["Identifie"];
+	carats = new Map([
+		[1, 2],
+		[2, 2.75],
+		[3, 3.5],
+		[4, 4.25],
+		[5, 5],
+		[-1, 1]
+	]);
+	weightToExpected = new Map([
+		["Outil", new Map([
+			[1, {name: "Aiguille", stars: 0}],
+			[2, {name: "Poinçon", stars: 0}],
+			[3, {name: "Marteau de joaillier", stars: 0}],
+			[5, {name: "Ciseau à bois", stars: 0}],
+			[20, {name: "Marteau de forgeron / de tailleur", stars: 0}],
+			[40, {name: "Hache de bûcheron", stars: 3}],
+			[60, {name: "Creuset", stars: 0}]
+		])],
+		["Anneau", new Map([
+			[2, {name: "Anneau standard", stars: 3}]
+		])],
+		["Arme 1 Main", new Map([
+			[5, {name: "Coutelas (os, pierre) / Corchet / Dague / Griffes / Racine / Torche", stars: 1}],
+			[7.5, {name: "Epée de bois / Fouet / Gantelet", stars: 4}],
+			[10, {name: "Epée courte / Hachette / Gourdin", stars: 0}],
+			[15, {name: "Cimeterre en pierre / Épieu / Gourdin clouté / Hache de lancer / Kama / Lame / Lame d'os / Machette / Pic", stars: 2}],
+			[20, {name: "Épée longue / Hache d'armes / Masse d'armes", stars: 0}],
+			[25, {name: "Fléau d'armes", stars: 0}],
+			[30, {name: "Épée bâtarde / Hache de guerre naine / Marteau de guerre / Morgenstern", stars: 1}]
+		])],
+		["Arme 2 mains", new Map([
+			[8, {name: "Bâton lesté / Bâtons de parade", stars: 0}],
+			[10, {name: "Arc court", stars: 3}],
+			[15, {name: "Arc composite / Arc long / Bâton de combat / Hachoir en bois", stars: 4}],
+			[20, {name: "Bâton de feu", stars: 5}],
+			[30, {name: "Arbalète légère / Épée à deux mains / Grande hache / Hache de bataille / de guerre / en os", stars: 3}],
+			[35, {name: "Arbalète composite / Chaîne cloutée", stars: 4}],
+			[40, {name: "Massue trolle, Pioche, Vouge", stars: 2}],
+			[45, {name: "Arbalète lourde, Faux", stars: 4}],
+			[50, {name: "Vorpale", stars: 5}],
+			[60, {name: "Lance-feu", stars: 5}]
+		])],
+		["Armure", new Map([
+			[3, {name: "Vêtement léger", stars: 0}],
+			[5, {name: "Pagne en cuir", stars: 0}],
+			[7, {name: "Vêtement lourd", stars: 0}],
+			[12, {name: "Armure légère", stars: 1}],
+			[20, {name: "Armure intermédiaire", stars: 1}],
+			[40, {name: "Armure lourde", stars: 3}],
+			[60, {name: "Armure très lourde", stars: 2}]
+		])],
+		["Baguette", new Map([
+			[3, {name: "Baguette standard", stars: 1}]
+		])],
+		["Bijou", new Map([
+			[5, {name: "Bijou standard", stars: 3}]
+		])],
+		["Bottes", new Map([
+			[3, {name: "Jambières en fourrure / Sandales", stars: 0}],
+			[5, {name: "Bottes", stars: 0}],
+			[15, {name: "Jambières lourdes", stars: 3}]
+		])],
+		["Bouclier", new Map([
+			[5, {name: "Targe", stars: 0}],
+			[10, {name: "Bouclier léger", stars: 3}],
+			[20, {name: "Bouclier lourd", stars: 3}],
+			[35, {name: "Bouclier à pointes", stars: 5}]
+		])],
+		["Casque", new Map([
+			[3, {name: "Casque léger", stars: 0}],
+			[7, {name: "Casque à cornes", stars: 0}],
+			[10, {name: "Casque à pointes", stars: 3}],
+			[15, {name: "Heaume", stars: 3}]
+		])],
+		["Talisman", new Map([
+			[3, {name: "Talisman standard", stars: 2}],
+			[5, {name: "Gorgeron en métal", stars: 1}],
+			[6, {name: "Collier de dents", stars: 0}]
+		])]
+	]);
+	nameToExpectedMaterial: Map<string, string> = new Map([
+		["Anapside", ""],
+		["Anneau Barbare", ""],
+		["Anneau de Courage", ""],
+		["Anneau de dureté", ""],
+		["Anneau de Protection", ""],
+		["Anneau de Sagesse", ""],
+		["Anneau de Savoir", ""],
+		["Anneau de voyage", ""],
+		["Anneau Spirituel", ""],
+		["Anneau Trollique", ""],
+		["Crochet", "Fer"],
+		["Dague", "Fer"],
+		["Epée bâtarde", "Fer"],
+		["Epée courte", "Fer"],
+		["Epée de bois", "Bois"],
+		["Epée longue", "Fer"],
+		["Epieu", "Bois"],
+		["Fléau d'armes", "Fer"],
+		["Fouet", "Cuir"],
+		["Gantelet", "Fer"],
+		["Gourdin", "Bois"],
+		["Gourdin clouté", "Bois"],
+		["Griffes", "Fer"],
+		["Grosse racine", "Bois"],
+		["Hache d'armes", "Fer"],
+		["Hache de guerre naine", "Fer"],
+		["Hache de lancer", "Fer"],
+		["Hachette", "Fer"],
+		["Kama", "Fer"],
+		["Lame", "Fer"],
+		["Machette", "Fer"],
+		["Marteau de guerre", "Bois"],
+		["Masse d'arme", "Bois"],
+		["Morgenstern", "Fer"],
+		["Pic", "Fer"],
+		["Torche", "Bois"],
+		["Arbalète composite", "Bois"],
+		["Arbalète légère", "Bois"],
+		["Arbalète lourde", "Bois"],
+		["Arc composite", "Bois"],
+		["Arc court", "Bois"],
+		["Arc long", "Bois"],
+		["Baton de combat", "Fer"],
+		["Bâton de feu", "Fer"],
+		["Bâton lesté", "Bois"],
+		["Bâtons de parade", "Bois"],
+		["Chaîne cloutée", "Fer"],
+		["Epée à deux mains", "Fer"],
+		["Espadon", "Fer"],
+		["Faux", "Fer"],
+		["Grande hache", "Fer"],
+		["Hache de bataille", "Fer"],
+		["Hache de guerre", "Fer"],
+		["Hachoir en bois", "Bois"],
+		["Lance-feu", "Fer"],
+		["Massue trolle", "Bois"],
+		["Pioche", "Fer"],
+		["Vorpale", "Fer"],
+		["Vouge", "Fer"],
+		["Cape", "Tissus"],
+		["Cape", "Tissus"],
+		["Culotte en cuir", "Cuir"],
+		["Robe de sorcier", "Tissus"],
+		["Tablier d'artisan", "Cuir"],
+		["Tunique", "Tissus"],
+		["Pagne en cuir", "Cuir"],
+		["Cotte d'ouvrier", "Fer"],
+		["Manteau de mage", "Tissus"],
+		["Manteau de sorcier", "Tissus"],
+		["Pagne de mailles", "Fer"],
+		["Toge", "Tissus"],
+		["Toison", "Cuir"],
+		["Armure de cuir", "Cuir"],
+		["Brigandine", "Cuir"],
+		["Chemise d'Alowin", "Cuir"],
+		["Plastron", "Fer"],
+		["Chemise de mailles", "Fer"],
+		["Cuir bouilli", "Cuir"],
+		["Cuirasse", "Fer"],
+		["Gambison", "Cuir"],
+		["Tunique d'écailles", "Fer"],
+		["Armure de bois", "Bois"],
+		["Cotte de mailles", "Fer"],
+		["Cuirasse d'écailles", "Fer"],
+		["Armure d'anneaux", "Fer"],
+		["Armure de plates", "Fer"],
+		["Clibanion", "Fer"],
+		["Crevice", "Fer"],
+		["Haubert de mailles", "Fer"],
+		["Baguette de Golbenstein", "Bois"],
+		["Baguette de Tsoin Tsoin", "Bois"],
+		["Baguette de Zénie", "Bois"],
+		["Noire baguette", "Bois"],
+		["Ankh Spectral", ""],
+		["Bijou-Fétiche", ""],
+		["Bijou-Totem", ""],
+		["Breloques Familiales", ""],
+		["Bricole d'Alowin", ""],
+		["Collier des Anciens", ""],
+		["Diadème Nébuleux", ""],
+		["Fibule Champêtre", ""],
+		["Magatama", ""],
+		["Médaillon Protecteur", ""],
+		["Menthalite", ""],
+		["Pendentif-Chasseur", ""],
+		["Phalère Epineuse", ""],
+		["Tiare Obscure", ""],
+		["Bottes", "Cuir"],
+		["Jambières en bois", "Bois"],
+		["Jambières en cuir", "Cuir"],
+		["Jambières en mailles", "Fer"],
+		["Jambières en métal", "Fer"],
+		["Sandales", "Cuir"],
+		["Targe", "Fer"],
+		["Clipeus", "Cuir"],
+		["Peltè", "Bois"],
+		["Rondache en bois", "Bois"],
+		["Aspis", "Bois"],
+		["Ecu en bois", "Bois"],
+		["Ecu en métal", "Fer"],
+		["Rondache en métal", "Fer"],
+		["Bouclier à pointes", "Fer"],
+		["Bacinet", "Fer"],
+		["Barbute", "Fer"],
+		["Cagoule", "Tissus"],
+		["Casque à cornes", "Fer"],
+		["Casque à pointes", "Fer"],
+		["Casque en cuir", "Cuir"],
+		["Casque en métal", ""],
+		["Cerebro", "Fer"],
+		["Chapeau pointu", "Tissus"],
+		["Heaume", "Fer"],
+		["Lorgnons", "Verre"],
+		["Masque d'Alowin", "Tissus"],
+		["Turban", "Tissus"],
+		["Amulette magnétique ", ""],
+		["Attrape-rêves", ""],
+		["Aurine", ""],
+		["Branches tressées", ""],
+		["Cercle parfait", ""],
+		["Colletin", ""],
+		["Collier à fleurs", ""],
+		["Collier à pointes", "Fer"],
+		["Gorgerin", ""],
+		["Gorgeron en cuir", "Cuir"],
+		["Gorgeron en métal", "Fer"],
+		["Jaseran", ""],
+		["Mâlâ", ""],
+		["Pectoral", ""],
+		["Phylactère", ""],
+		["Ruban d'Alowin", ""],
+		["Aiguille", "Fer"],
+		["Ciseau à bois", "Bois"],
+		["Creuset", "Fer"],
+		["Hache de bûcheron", "Fer"],
+		["Marteau de forgeron", "Fer"],
+		["Marteau de joaillier", "Fer"],
+		["Marteau de tailleur", "Pierre"],
+		["Poinçon", "Fer"],
 
-  constructor(injector: Injector) {
-    super(injector);
-  }
+		["Bâton lesté / Bâtons de parade", "Bois"],
+		["Baguette standard", "Bois"]
+	]);
+	nameToExpectedMaterialContains = [
+		{name: "en Pierre", materiau: "Pierre"}
+	];
 
-  public get(force: boolean = false): Observable<GuildPlaceItemsTypings[]> {
-    if (localStorage.getItem("guildplaceitems") && !force) {
-      console.log("get local");
-      const json = JSON.parse(localStorage.getItem("guildplaceitems"));
-      return Observable.of(json);
-    } else {
-      console.log("get distant");
-      const token = LoginService.getToken();
-      if (token) {
-        return this.http.get("https://www.chifret.be/gobkipu/services/guildplace.php?key=" + token.clan + "&id=" + token.id, {responseType: 'text'})
-          .map((res: any) => {
-            const json = CsvUtils.getJson<GuildPlaceItemsTypings>(res, this.numerics, this.floats, this.dates, this.booleans);
-            this.enrichment(json);
-            localStorage.setItem("guildplaceitems", JSON.stringify(json));
-            return json;
-          });
-      } else {
-        return Observable.empty();
-      }
-    }
-  }
+	constructor(injector: Injector) {
+		super(injector);
+	}
 
-  private enrichment(json: GuildPlaceItemsTypings[]): GuildPlaceItemsTypings[] {
-    for (let i = 0; i < json.length; i++) {
-      if (json[i].Type === "Matériau" && !json[i].Qualite) {
-        json[i].Qualite = -1;
-      }
-      if (json[i].Qualite && !(["Composant", "Fleur"].indexOf(json[i].Type) > -1)) {
-        if (!json[i].Matiere) {
-          json[i].Matiere = json[i].Nom === "Rondin" ? "Bois" : json[i].Nom;
-        }
-        json[i].Category = "Matériaux";
-        json[i].Carats = json[i].Taille * this.carats.get(json[i].Qualite);
-        json[i].Desc = null;
-      } else if (["Nourriture", "Potion", "Outil", "Corps"].indexOf(json[i].Type) > -1) {
-        json[i].Category = json[i].Type;
-        if (!(json[i].Type === "Potion")) {
-          json[i].Desc = null;
-        }
-        if (["Nourriture", "Potion", "Outil"].indexOf(json[i].Type) > -1) {
-          json[i].Taille = null;
-        }
-      } else if (["Fleur", "Composant"].indexOf(json[i].Type) > -1) {
-        json[i].Category = "Compo & Fleurs";
-        json[i].Desc = null;
-      } else {
-        json[i].Category = "Équipement";
-        json[i].Taille = null;
-        if (json[i].Matiere) {
-          json[i].Nom = json[i].Nom + " en " + json[i].Matiere;
-          json[i].Matiere = null;
-        }
-      }
-      if (json[i].Poids) {
-        json[i].Poids = Math.round((json[i].Poids / 60) * 100) / 100;
-      }
+	public get(force: boolean = false): Observable<GuildPlaceItemsTypings[]> {
+		if (localStorage.getItem("guildplaceitems") && !force) {
+			console.log("get local");
+			const json = JSON.parse(localStorage.getItem("guildplaceitems"));
+			this.enrichment(json);
+			return Observable.of(json);
+		} else {
+			console.log("get distant");
+			const token = LoginService.getToken();
+			if (token) {
+				return this.http.get("https://www.chifret.be/gobkipu/services/guildplace.php?key=" + token.clan + "&id=" + token.id, {responseType: 'text'})
+					.map((res: any) => {
+						const json = CsvUtils.getJson<GuildPlaceItemsTypings>(res, this.numerics, this.floats, this.dates, this.booleans);
+						// this.enrichment(json);
+						localStorage.setItem("guildplaceitems", JSON.stringify(json));
+						this.enrichment(json);
+						return json;
+					});
+			} else {
+				return Observable.empty();
+			}
+		}
+	}
 
-      if (!json[i].Identifie && ["Équipement", "Outil"].indexOf(json[i].Category) > -1) {
-        if (this.WeightToExpected.get(json[i].Type)) {
-          if (this.WeightToExpected.get(json[i].Type).get(json[i].Poids)) {
-            json[i].Expected = this.WeightToExpected.get(json[i].Type).get(json[i].Poids).name;
-            json[i].Stars = this.WeightToExpected.get(json[i].Type).get(json[i].Poids).stars;
-          } else {
-            json[i].Expected = "???";
-            json[i].Stars = 5;
-          }
-        } else {
-          //console.log("Weight to expected " + json[i].Type + " not done");
-        }
-      }
-    }
-    return json;
-  }
+	private enrichment(json: GuildPlaceItemsTypings[]): GuildPlaceItemsTypings[] {
+		for (let i = 0; i < json.length; i++) {
+
+			// fix rondins
+			if (json[i].Type === "Matériau" && json[i].Nom === "Rondin" && !json[i].Qualite) {
+				json[i].Qualite = -1;
+				json[i].Matiere = "Bois";
+			}
+
+			// fix poids
+			if (json[i].Poids) {
+				json[i].Poids = Math.round((json[i].Poids / 60) * 100) / 100;
+			}
+
+			// category, desc, types
+			if (json[i].Qualite) {
+				if (["Composant", "Fleur"].indexOf(json[i].Type) > -1) {
+					json[i].Category = "Compo & Fleurs";
+				}
+				else {
+					json[i].Category = "Matériaux";
+				}
+				json[i].Desc = null;
+				json[i].Type = null;
+			} else if (["Nourriture", "Potion", "Outil", "Corps"].indexOf(json[i].Type) > -1) {
+				json[i].Category = json[i].Type;
+				if (!(json[i].Type === "Potion")) {
+					json[i].Desc = null;
+				}
+				if (["Nourriture", "Potion", "Outil"].indexOf(json[i].Type) > -1) {
+					json[i].Taille = null;
+				}
+				json[i].Type = null;
+			} else {
+				json[i].Category = "Équipement";
+				json[i].Taille = null;
+				if (json[i].Matiere) {
+					json[i].Nom = json[i].Nom + " en " + json[i].Matiere;
+				}
+			}
+
+			// waight expectation
+			if (!json[i].Identifie && ["Équipement", "Outil"].indexOf(json[i].Category) > -1) {
+				if (this.weightToExpected.get(json[i].Type)) {
+					if (this.weightToExpected.get(json[i].Type).get(json[i].Poids)) {
+						json[i].Nom = this.weightToExpected.get(json[i].Type).get(json[i].Poids).name;
+						json[i].Stars = this.weightToExpected.get(json[i].Type).get(json[i].Poids).stars;
+					} else {
+						json[i].Nom = "???";
+						json[i].Stars = 5;
+					}
+				} else {
+					console.log("Weight to expected " + json[i].Type + " not done");
+				}
+			}
+
+			if (["Équipement", "Outil"].indexOf(json[i].Category) > -1) {
+
+				// material expectation
+				let mat = this.nameToExpectedMaterial.get(json[i].Nom);
+
+				// name to material
+				if (!mat) {
+					this.nameToExpectedMaterialContains.forEach((materialContained) => {
+						if (json[i].Nom.indexOf(materialContained.name) > -1) {
+							mat = materialContained.materiau;
+						}
+					})
+				}
+
+				json[i].Matiere = mat;
+				if (json[i].Matiere) {
+					switch (json[i].Matiere) {
+						case "Bois":
+							json[i].Taille = Math.max(1, Math.floor(json[i].Poids * 0.9));
+							break;
+						case "Cuir":
+							json[i].Taille = Math.max(1, Math.floor(json[i].Poids * 0.225));
+							break;
+						case "":
+						case null:
+							json[i].Taille = null;
+							break;
+						//default:
+						case "Fer":
+						case "Pierre":
+						case "Tissus":
+							json[i].Taille = Math.max(1, Math.floor(json[i].Poids * 0.45));
+							break;
+					}
+				}
+			}
+
+			// carats
+			if (json[i].Taille && json[i].Matiere) {
+				json[i].Carats = json[i].Taille * (json[i].Qualite ? this.carats.get(json[i].Qualite) : 3);
+			}
+
+		}
+		return json;
+	}
 }
--- src/app/identify/identify.component.ts
+++ src/app/identify/identify.component.ts
@@ -1,57 +1,137 @@
-import { Component, Injector, ViewChild, OnInit } from '@angular/core';
+import {Component, Injector, OnInit, ViewChild} from '@angular/core';
 
-import { GuildplacesService } from './../core/services/identify/guildplaces.service';
-import { ItemsService } from '../core/services/identify/items.service';
-import { CollectionView } from 'wijmo/wijmo';
-import { Subscription } from 'rxjs';
-import { DataMap } from 'wijmo/wijmo.grid';
-import { QeosGridComponent } from '../core/components/QeosGrid/qeosgrid.component';
+import {GuildplacesService} from './../core/services/identify/guildplaces.service';
+import {ItemsService} from '../core/services/identify/items.service';
+import {CollectionView, GroupDescription, PropertyGroupDescription, SortDescription} from 'wijmo/wijmo';
+import {Subscription} from 'rxjs';
+import {DataMap} from 'wijmo/wijmo.grid';
+import {QeosGridComponent} from '../core/components/QeosGrid/qeosgrid.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './identify.component.html'
+	selector: 'app-root',
+	templateUrl: './identify.component.html'
 })
 export class IdentifyComponent implements OnInit {
 
 	busy = false;
 	text = "Refresh";
-    cvMain: CollectionView = null;
-    subsMain: Subscription;
+	cvMain: CollectionView = null;
+	subsMain: Subscription;
 
-    dtmQuality = new DataMap([
-        { key: -1, value: "---" },
-        { key: 0, value: "" },
-        { key: 1, value: "Médiocre" },
-        { key: 2, value: "Moyenne" },
-        { key: 3, value: "Normale" },
-        { key: 4, value: "Bonne" },
-        { key: 5, value: "Exceptionnelle" }
-    ], "key", "value");
+	dtmQuality = new DataMap([
+		{key: -1, value: "---"},
+		{key: 0, value: ""},
+		{key: 1, value: "Médiocre"},
+		{key: 2, value: "Moyenne"},
+		{key: 3, value: "Normale"},
+		{key: 4, value: "Bonne"},
+		{key: 5, value: "Exceptionnelle"}
+	], "key", "value");
 
-    @ViewChild("grid") grid: QeosGridComponent;
+	@ViewChild("grid") grid: QeosGridComponent;
 
-    constructor(protected injector: Injector,
-        protected itemsService: ItemsService,
-        protected guildplacesService: GuildplacesService) {
-        this.subsMain = this.guildplacesService.get().subscribe((res) => {
-            this.cvMain = new CollectionView(res);
-        });
-    }
+	constructor(protected injector: Injector,
+				protected itemsService: ItemsService,
+				protected guildplacesService: GuildplacesService) {
+		this.subsMain = this.guildplacesService.get().subscribe((res) => {
+			this.cvMain = new CollectionView(res);
+		});
+	}
 
-    initGrid() {
+	initGrid() {
 
-    }
+	}
 
-    ngOnInit(): void {
-        this.grid.filter.filterColumns = [
-            "Identifie", "Category", "Type", "Matiere"
-        ];
-        this.grid.headersVisibility = 1;
-    }
+	ngOnInit(): void {
+		this.grid.filter.filterColumns = [
+			"Identifie", "Category", "Type", "Matiere"
+		];
+		this.grid.headersVisibility = 1;
+	}
 
-    refresh(){
-      this.subsMain = this.guildplacesService.get(true).subscribe((res) => {
-        this.cvMain = new CollectionView(res);
-      });
-    }
+	refresh() {
+		this.subsMain = this.guildplacesService.get(true).subscribe((res) => {
+			this.cvMain = new CollectionView(res);
+		});
+	}
+
+	preset(preset: string): void {
+		switch (preset) {
+			case "e":
+				// filter
+				this.grid.filter.filterDefinition = JSON.stringify({
+					"defaultFilterType": 3,
+					"filters": [
+						{"binding": "Identifie", "type": "value", "filterText": "", "showValues": {"true": true}},
+						{"binding": "Category", "type": "value", "filterText": "", "showValues": {"Équipement": true}}
+					]
+				});
+				// sort
+				this.cvMain.sortDescriptions.clear();
+				this.cvMain.sortDescriptions.push(new SortDescription('Type', true));
+				this.cvMain.sortDescriptions.push(new SortDescription('Stars', false));
+				// group
+				this.cvMain.groupDescriptions.clear();
+				this.cvMain.groupDescriptions.push((new PropertyGroupDescription('Type') as GroupDescription));
+				// collapse
+				this.grid.collapseGroupsToLevel(this.grid.collectionView.groupDescriptions.slice().length);
+				break;
+			case "i":
+				// filter
+				this.grid.filter.filterDefinition = JSON.stringify({
+					"defaultFilterType": 3,
+					"filters": [{"binding": "Identifie", "type": "value", "filterText": "", "showValues": {"false": true}}]
+				});
+				// sort
+				this.cvMain.sortDescriptions.clear();
+				this.cvMain.sortDescriptions.push(new SortDescription('Stars', false));
+				// group
+				this.cvMain.groupDescriptions.clear();
+				// collapse
+				this.grid.collapseGroupsToLevel(this.grid.collectionView.groupDescriptions.slice().length);
+				break;
+			case "r":
+				// filter
+				this.grid.filter.filterDefinition = JSON.stringify({
+					"defaultFilterType": 3,
+					"filters": [
+						{"binding": "Identifie", "type": "value", "filterText": "", "showValues": {"true": true}},
+						{"binding": "Category", "type": "value", "filterText": "", "showValues": {"Équipement": true, "Outil": true}},
+						// {"binding": "Matiere", "type": "value", "filterText": "", "showValues": {"": false}}
+						{"binding": "Matiere", "type": "value", "filterText": "", "showValues": {"Bois":true,"Cuir":true,"Fer":true,"Pierre":true,"Tissus":true}}
+					]
+				});
+				// sort
+				this.cvMain.sortDescriptions.clear();
+				this.cvMain.sortDescriptions.push(new SortDescription('Type', true));
+				this.cvMain.sortDescriptions.push(new SortDescription('Carats', false));
+				// group
+				this.cvMain.groupDescriptions.clear();
+				this.cvMain.groupDescriptions.push((new PropertyGroupDescription('Matiere') as GroupDescription));
+				// collapse
+				this.grid.collapseGroupsToLevel(0);
+				break;
+			case "m":
+				// filter
+				this.grid.filter.filterDefinition = JSON.stringify({
+					"defaultFilterType": 3,
+					"filters": [{"binding": "Category", "type": "value", "filterText": "", "showValues": {"Matériaux": true}}]
+				});
+				// sort
+				this.cvMain.sortDescriptions.clear();
+				this.cvMain.sortDescriptions.push(new SortDescription('Type', true));
+				this.cvMain.sortDescriptions.push(new SortDescription('Carats', false));
+				// group
+				this.cvMain.groupDescriptions.clear();
+				this.cvMain.groupDescriptions.push((new PropertyGroupDescription('Matiere') as GroupDescription));
+				// collapse
+				this.grid.collapseGroupsToLevel(0);
+				break;
+			default:
+				console.log(this.grid.filter.filterDefinition);
+				console.log(this.cvMain.sortDescriptions);
+				console.log(this.cvMain.groupDescriptions);
+		}
+		this.grid.select(0, 0);
+	}
 }
